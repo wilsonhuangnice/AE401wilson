ed99ae8aaf3696f95b8e2188a6770e24ee168b25
wilsonhuangnice <71084205+wilsonhuangnice@users.noreply.github.com>
2020-12-05 11:49:30 +0800

    Add files via upload

---
+++ 1205.py
@@ -0,0 +1,22 @@
+
+
+
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+def buildPyramid(x,y,z,base):
+ 
+    
+    height = (base//2) + 1
+    for i in range(height):
+        x1 = x + i
+        y1 = y + i
+        z1 = z + i
+        
+        x2 = x + base - i
+        z2 = z + base - i
+        mc.setBlocks(x1, y1, z1, x2, y, z2, 133)
+
+x,y,z = mc.player.getTilePos()
+base = int(input('enter base'))
+buildPyramid(x,y,z,base)
